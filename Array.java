21fe3fd41f0fd851da82d276a1f18b7f046c86ee
Kunals1436 <krunalshende1436@gmail.com>
2025-05-26 00:06:04 +0530

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Array {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array:");
+        int marks[]=new int[5];
+        for(int i=0;i<marks.length;i++){
+            marks[i]=sc.nextInt();
+        }
+        System.out.println("The elements in the array are:");
+        for(int i=0;i<marks.length;i++){
+            System.out.print(marks[i]+" ");
+        }
+    }
+}
