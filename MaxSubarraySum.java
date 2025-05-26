1573523f8e8c1cd882e9dade0059287471188f2e
Kunals1436 <krunalshende1436@gmail.com>
2025-05-27 01:36:22 +0530

    Sub Array Problems

--- MaxSubarraySum.java
+++ MaxSubarraySum.java
@@ -1,25 +1,33 @@
-public class SubArray {
+public class MaxSubarraySum {
     public static void subArray(int arr[]){
         int n=arr.length;
         int start;
         int end;
+        int currentSum=0;
+        int maxSum = Integer.MIN_VALUE; // Initialize to the smallest possible integer
         for(int i=0;i<n;i++){
             start=i;
             for(int j=i;j<n;j++){
                 end =j;
-        
-                // System.out.print("Subarray from index " + arr[i] + " to " + j + ": ");
+                currentSum = 0; // Reset current sum      
                 for(int k=start;k<=end;k++){
                     System.out.print(arr[k] + " ");
+                    currentSum += arr[k]; // Calculate the sum of the current subarray
                 }
                 System.out.println();
+                System.out.println("Current Sum:"+currentSum);
             }
             System.out.println();
+            if(currentSum > maxSum) {
+                maxSum = currentSum; // Update maxSum 
+            }
         }
+
+        System.out.println("Maximum subarray sum is: " + maxSum);
     }
     public static void main(String[] args) {
-        int arr[]={2,4,6,8,10};
+        int arr[]={1,-2,6,-1,3};
         System.out.println("The subarrays in the array are:");
         subArray(arr);
     }
-}
+}   
---
+++ MaxSubarraySum_Prefixsum.java
@@ -0,0 +1,38 @@
+public class MaxSubarraySum_Prefixsum {
+public static void subArray(int arr[]){
+        int n=arr.length;
+        int start;
+        int end;
+        int currentSum=0;
+        int maxSum = Integer.MIN_VALUE; // Initialize to the smallest possible integer
+        int prefixSum[] = new int[n];
+        prefixSum[0] = arr[0]; // Initialize the first element of prefixSum
+        for(int i=1;i<n;i++){
+            prefixSum[i] = prefixSum[i-1] + arr[i]; // Calculate prefix sum
+        }
+        for(int i=0;i<n;i++){
+            start=i;
+            for(int j=i;j<n;j++){
+                end =j;
+                currentSum = 0; // Reset current sum
+                if(start == 0) {
+                    currentSum = prefixSum[end]; // If start is 0, take the prefix sum directly
+                } else {
+                    currentSum = prefixSum[end] - prefixSum[start - 1]; // Calculate sum using prefix sums
+                }    
+              
+            }
+            System.out.println();
+            if(currentSum > maxSum) {
+                maxSum = currentSum; // Update maxSum 
+            }
+        }
+        System.out.println("Maximum subarray sum is: " + maxSum);
+    }        
+    public static void main(String[] args) {
+        int arr[]={1,-2,6,-1,3};
+        System.out.println("The subarrays in the array are:");
+        subArray(arr);
+    }    
+    
+}
