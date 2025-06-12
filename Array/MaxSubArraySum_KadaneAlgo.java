de7f1a7cf4df2dde0820d6e6e6c6a23340e14362
Kunals1436 <krunalshende1436@gmail.com>
2025-06-12 23:39:54 +0530

    Maximum Sub Array(Kadane's Algo)

--- Array/MaxSubArraySum_KadaneAlgo.java
+++ Array/MaxSubArraySum_KadaneAlgo.java
@@ -40,7 +40,5 @@ public class MaxSubArraySum_KadaneAlgo {
     public static void main(String[] args) {
         int[] arr={-2,-3,4,-1,-2,1,5,-3};
         maxSum(arr);
-        int[] arr2={-1,-2,-3,-4};
-        maxSum(arr2);
     }
 }
